727071258a0e3dbb8ad0359c76263a365a8bb2b7
AlesiaVaranovich <alesia.varanovich@hhu.de>
2014-04-13 20:47:27 +0200

    Test file

---
+++ test1.java
@@ -0,0 +1,12 @@
+
+public class test1 {
+	public static int distanse(int a, int b) {
+int c;
+c=a-b;
+return (c);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("otvet "+distanse(42,5));
+	}
+}
